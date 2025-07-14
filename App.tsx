b336587103367a9b2ae0408fca0e62af72091af6
Thuno <chienvd@wini.vn>
2025-07-14 19:03:44 +0700

    ....

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native"
+import { } from "wini-mobile-component"
+
+const App = () => {
+    return  <View>
+    </View>
+}
+
+export default App
--- app/(tabs)/settings.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>⚙️ Settings</Text>
-        </View>
-    );
-}
